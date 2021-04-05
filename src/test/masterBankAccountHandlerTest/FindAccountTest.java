bc8caa19543fbe86457382e474e1246fa92d1cc2
Anthony Huang <Huang.Anthony@outlook.com>
2021-04-05 18:06:30 -0400

    Modified findaccounttest cases

--- src/test/masterBankAccountHandlerTest/FindAccountTest.java
+++ src/test/masterBankAccountHandlerTest/FindAccountTest.java
@@ -11,13 +11,14 @@ import main.java.masterbank.*;
 public class FindAccountTest {
     @Test
     public void findAccountTest1() {
-    	//Initialize the objects used in the test
+    	// Initialize the objects used in the test
         MasterBankAccountHandler masterBankAccountHandler = new MasterBankAccountHandler();
         ArrayList<MasterBankAccountData> accountsList = new ArrayList<MasterBankAccountData>();
         MasterBankAccountData testAccount = new MasterBankAccountData(0, "John", "", 0.0f, 0, false);
         accountsList.add(testAccount);
         masterBankAccountHandler.setAccounts(accountsList);
 
+        // Test
         MasterBankAccountData masterbankAccount = masterBankAccountHandler.findAccount("John",0);
         assertEquals("John", masterbankAccount.getName());
         assertEquals(0, masterbankAccount.getNumber());
@@ -25,9 +26,14 @@ public class FindAccountTest {
 
     @Test
     public void findAccountTest2(){
+        // Initialize the objects used in the test
         MasterBankAccountHandler masterBankAccountHandler = new MasterBankAccountHandler();
-        masterBankAccountHandler.readFile("src/test/resources/master_bank_accounts.txt");
+        ArrayList<MasterBankAccountData> accountsList = new ArrayList<MasterBankAccountData>();
+        MasterBankAccountData testAccount = new MasterBankAccountData(0, "A", "", 0.0f, 0, false);
+        accountsList.add(testAccount);
+        masterBankAccountHandler.setAccounts(accountsList);
 
+        // Test
         MasterBankAccountData masterbankAccount = masterBankAccountHandler.findAccount("John",0);
         assertNull(masterbankAccount);
     }

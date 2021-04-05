e1580a5f7964b87fc4b76e4ce1a030ab18110193
Nandor Gallo <nandor.gallo@uoit.net>
2021-04-05 17:28:45 -0400

    Update

--- src/test/masterBankAccountHandlerTest/LoopTest.java
+++ src/test/masterBankAccountHandlerTest/LoopTest.java
@@ -14,16 +14,26 @@ public class LoopTest {
     	 MasterBankAccountHandler masterBankAccountHandler = new MasterBankAccountHandler();
     	 assertEquals("ERROR: Master bank account file does not exist", masterBankAccountHandler.readFile("wrongfile.txt"));
      }
-    //verify results of first if check. Compares output when File does exist
+     
+     
+
+   //Checks condition of an empty file
     @Test
     public void testLoop() throws IOException {
-    	 MasterBankAccountHandler masterBankAccountHandler = new MasterBankAccountHandler();
-        //Checks condition of an empty file
+    	MasterBankAccountHandler masterBankAccountHandler = new MasterBankAccountHandler();
         assertEquals("", masterBankAccountHandler.readFile("test_Empty_file.txt"));
-        //Checks condition of a file with one line 
-        
+    }
+  
+    //Checks condition of a file with one line
+    @Test
+    public void testLoop1() throws IOException {
+    	MasterBankAccountHandler masterBankAccountHandler = new MasterBankAccountHandler();
 	assertEquals("00001 John Doe             A 00000100 0001 true", masterBankAccountHandler.readFile("src/test/resources/looptest1.txt"));
-        //cond twice
+    }
+    //Checks condition of a file with two lines
+    @Test
+    public void testLoop2() throws IOException {
+    	MasterBankAccountHandler masterBankAccountHandler = new MasterBankAccountHandler();
         assertEquals("00001 John Doe             A 00000100 0001 true 00002 John Dae             A 00000300 0002 false ",masterBankAccountHandler.readFile("src/test/resources/looptest2.txt"));
     }
    }

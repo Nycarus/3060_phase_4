21cdfa80cf400071d5a4c0c0b7de725575f9de90
digital-drreams <ben.armstrong@ontariotechu.net>
2021-04-05 16:16:54 -0400

    Changes findAccountTest1() to no longer rely on file IO This way, if the implementation of file IO is somehow not functional or incorrect, findAccountTest will still correctly test the methods involved.

--- src/test/masterBankAccountHandlerTest/FindAccountTest.java
+++ src/test/masterBankAccountHandlerTest/FindAccountTest.java
@@ -1,14 +1,21 @@
 package test.masterBankAccountHandlerTest;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import main.java.masterbank.*;
 
 public class FindAccountTest {
 
     @Test
     public void findAccountTest1() {
+    	//Initialize the objects used in the test
         MasterBankAccountHandler masterBankAccountHandler = new MasterBankAccountHandler();
-        masterBankAccountHandler.readFile("src/test/resources/master_bank_accounts.txt");
+        ArrayList<MasterBankAccountData> accountsList = new ArrayList<MasterBankAccountData>();
+        MasterBankAccountData testAccount = new MasterBankAccountData(0, "John", "", 0.0f, 0, false);
+        accountsList.add(testAccount);
+        masterBankAccountHandler.setAccounts(accountsList);
 
         MasterBankAccountData masterbankAccount = masterBankAccountHandler.findAccount("John",0);
         assertEquals("John", masterbankAccount.name);
